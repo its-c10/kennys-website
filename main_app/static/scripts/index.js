975644ac4af6ab6c1815eb468effe444b70bd9d1
c10coding <caleb.ja.owens@gmail.com>
2021-03-01 22:43:02 -0500

    Nav is done. Jumbo is almost done.

---
+++ main_app/static/scripts/index.js
@@ -0,0 +1,16 @@
+$(function () {
+    $(document).scroll(function () {
+
+        //let $leftSideJumbo = $("#left-side-jumbo-wrap");
+
+        let $nav = $("#nav");
+        let $linksInNav = $("#nav a");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $linksInNav.toggleClass('toggleLinkColor', $(this).scrollTop() > $nav.height());
+
+        //let $getStartedBtn = $("#nav-get-started");
+        //$getStartedBtn.toggleClass('displayed', ($(this).scrollTop() - 150) > $leftSideJumbo.height());
+
+    });
+
+});
